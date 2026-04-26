0798d6d07847d3f13c1611116abbea34da2703d8
D00M777 <dbar2604@gmail.com>
2026-04-26 17:46:23 -0500

    Update counter_fsm_tb.v

--- counter_fsm_tb.v
+++ counter_fsm_tb.v
@@ -5,7 +5,7 @@
 // 
 // Create Date: 03/28/2023 01:35:20 PM
 // Design Name: 
-// Module Name: le9_tb
+// Module Name: 
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -19,33 +19,34 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module test;
-  reg en,clk,reset;
-  wire pulse;
-    
-  // Instantiate design under test
-  state_cntr U1(
-  .en(en), .clk(clk), .reset(reset), .pulse(pulse)
-  );
-    initial begin
-    en = 0;
-    clk = 0;
-    reset = 0;
-    #2
-    reset = 1;
-    #2 
-    reset = 0;
-    #6
-    en = 1;
-    #800
-    $finish;
-    //$display("Value of c in hex is %h", c);
-    //$display("Parity of c is %b", parity);
-  end
-  
-  always begin
-    #10
-    clk = ~clk;
-  end
-  
+module state_cntr_tb;
+
+reg clk, reset, en;
+wire ones_pulse;
+wire [7:0] ones_cnt;
+wire [7:0] tens_cnt;
+
+initial clk = 0;
+always begin
+    #5 //wait
+    clk = ~clk; //clock set up
+end
+// Ones place counter
+  state_cntr ones (.clk(clk), .reset(reset), .en(ones_en), .tc(8'd9), .count(ones_cnt), .pulse(ones_pulse));
+
+// Tens place counter (0-3)
+state_cntr tens (.clk(clk), .reset(reset), .en(ones_pulse), .tc(8'd3), .count(tens_cnt), .pulse());
+
+initial begin
+    reset = 1; //reset
+    en = 0; //enable off
+    #20; //wait
+
+    reset = 0; //reset off
+    en = 1; // enable on
+
+    #500; //wait
+    $finish; //end
+end
+
 endmodule
