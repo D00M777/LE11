60671df9ee393b7f7122fbc1f61e157051d927ea
D00M777 <dbar2604@gmail.com>
2026-04-26 17:47:54 -0500

    Update counter_fsm.v

--- counter_fsm.v
+++ counter_fsm.v
@@ -5,7 +5,7 @@
 // 
 // Create Date: 03/28/2023 01:35:37 PM
 // Design Name: 
-// Module Name: le9
+// Module Name: 
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -19,33 +19,30 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module state_cntr(
-    input en,
+
+module state_cntr (
     input clk,
     input reset,
+    input en,
+    input  [7:0] tc,
+    output reg [7:0] cnt,
     output pulse
-    );
-
-reg [7:0] cnt;
-reg [7:0] next_cnt;
+);
 wire ones_en = en && !(tens_cnt == 5 && ones_cnt == 4);
 
 // flip-flops (state memeory)
+
 always @(posedge clk or posedge reset) begin
-    if (reset==1'b1) cnt<=8'd0; // reset to zero 
-    else cnt <= next_cnt;
+    if (reset)
+        cnt <= 8'd0;
+    else if (en) begin
+        if (cnt == tc)
+            cnt <= 8'd0;
+        else
+            cnt <= cnt + 1'b1;
+    end
 end
 
-// next-state logic 
-always @(*) begin
-    if(en) begin // check for the enable 
-        if (cnt == 8'd15) next_cnt = 0; // if the counter reaches 15 go back to zero 
-        else next_cnt = cnt + 1; // new concept -- a counter! 
-    end 
-    else next_cnt = cnt; // if enable is low stay in the same state 
-end
-
-// output logic 
-assign pulse = (cnt == 8'd15); // at the last state send out a pulse 
+assign pulse = en && (cnt == tc);
 
 endmodule
